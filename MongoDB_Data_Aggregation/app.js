32899269dcd26ca01ec4418be841b3cfe695e318
hardyal-singh <hardyalsingh2122@gmail.com>
2024-02-17 22:38:06 +0530

    make chanage

--- MongoDB_Data_Aggregation/app.js
+++ MongoDB_Data_Aggregation/app.js
@@ -1,6 +1,7 @@
-
+import env from "dotenv"
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://hardyal:hardyal@cluster0.eh64wre.mongodb.net/?retryWrites=true&w=majority";
+env.config()
+const uri =process.env.MONGO_URI 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
